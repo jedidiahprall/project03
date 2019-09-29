c94d1447a34b6cae4ca3c2c7106e5ef5675b195b
jprall <jedidiah.prall@gmail.com>
2019-09-29 13:17:30 -0600

    Minor changes. Will work on it for an extended period of time tomorrow

--- src/JedidiahPrallNicholasRyan_03.java
+++ src/JedidiahPrallNicholasRyan_03.java
@@ -1,26 +1,34 @@
-/* 
-
-PHM 
-
-
+/** 
+  *
+  *Vocabulary Word:
+  *Suffuse:
+  *"to spread over or through in the manner of fluid or light : flush, fill" 
+  *
+  *
 */ 
 
-
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
-import java.io; 
+
 
 public class JedidiahPrallNicholasRyan_03 {
+
+   static ArrayList<Student> students;
+   static ArrayList<GradeItem> gradeItems;
+   
    static Toolkit tools = new Toolkit();
    public static void main(Strings[] args) throws IOException {
    
-   /* 
-   
-   Relevant Declarations go here 
-   
-   */ 
-   
-   final String INPUT_FILE = "Project_03_Input01.txt";
-   final String OUTPUT_FILE = "Project_03_Output.txt";
+   String xx = "";
+   Scanner scan = new Scanner(System.in);
+   students = new ArrayList<>();
+   gradeItems = new ArrayList<>();
+   System.out.print("Enter file number: ");
+   xx = scan.next();
+   final String INPUT_FILENAME = "Project_02_Input" + xx + ".txt";
+   File file = new File(INPUT_FILENAME);
    
    FileWriter outputDataFile = new FileWriter(OUTPUT_FILE);
    PrintWriter outputFile = new PrintWriter(outputDataFile);
@@ -40,6 +48,10 @@ organizing data, and finally printing the data to an output file.
 //*****************************************************************************
 
 public static void processInput () throws IllegalArgumentException {
+   
+   
+
+      
 
 }// End processInput
 

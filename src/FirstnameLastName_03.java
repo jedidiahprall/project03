bb18347a48e83cbeb390e638ff984e1b15f049b7
nryan100 <nicholasryan100@gmail.com>
2019-10-02 21:09:55 -0600

    Update FirstnameLastName_03.java

--- src/FirstnameLastName_03.java
+++ src/FirstnameLastName_03.java
@@ -1,40 +1,20 @@
 import java.io.File;
 import java.util.Scanner;
-
+import java.util.ArrayList;
 /**
  * 
  * @author 
  *
  */
- 
-
-
-
 public class FirstnameLastName_03 {
 	
-   
-	private static List listOfStudents;
-	private static List listOfGradeItems;
-	private static final String INPUT_FILE = null;
-	private static final String OUTPUT_FILE = null;
-   
-   
-   
-   //FileWriter outputDataFile = new FileWriter(OUTPUT_FILE);
-   //PrintWriter outputFile = new PrintWriter(outputDataFile);
-   
+	private static List<Student> listOfStudents;		// List of Student objects
+	private static List<GradeItem> listOfGradeItems;	// List of GradeItem objects
+	private static String INPUT_FILE = "Input.txt";  
+	private static final String OUTPUT_FILE = null; 
 	
 	/**
-	 * TODO:
-	 * - Open the input file. 
-	 * 	- Display an error message using the err (System.err.print*) stream if the file is not found
-	 * 	- Include the NAME of the missing file in the message (be user friendly)
-	 * - Read all lines of text (if it can be open)
-	 * 	- Use the split function to parse the line
-	 * 	- If the first field is STUDENT, call processStudentData method
-	 * 	 - Pass it the current line array
-	 * 	- Else, if the first field is GRADE ITEM, call the processGradeItemData method
-	 * 	 - Do the same as above	 	
+	 * 
 	 */
 	public static void processInput() {
 		int lineNum = 0; 	  // Indicates which line the reader is on
@@ -51,10 +31,10 @@ public class FirstnameLastName_03 {
 				workingLine = fileReader.nextLine().split(",");
 				
 				if (workingLine[0].equals("STUDENT")) {
-					processStudentData(workingLine, lineNum);
+					processStudentData(workingLine);
 					
 				} else if (workingLine[0].equals("GRADE ITEM")) {
-					processGradeItemData(workingLine, lineNum);
+					processGradeItemData(workingLine);
 					
 				} else {
 					System.err.println("\'STUDENT\' or \'GRADE ITEM\' token not found."
@@ -68,144 +48,156 @@ public class FirstnameLastName_03 {
 		} // End try / catch
 	} // End processInput
 	/**
-	 * TODO:
-	 * Does the following:
-	 * - Recieves an array of type string with student data 
-	 * 	- if the second field is ADD, do the following: (implemented the same as the second project)
-	 * 	- Create an instance of Student
-	 * 	- Use the TODO contains method in the List class to make sure Student is unique. 
-	 * 	 - If it's already in the list, display an error message that includes the student ID
-	 * 	- Call the TODO add method in the List class to add the Student object to the list of students. 
-	 * 	 - If the add method's return value indicates an error, display a message with the Student ID
-	 * 	- If the second field is DEL:
-	 * 	 - Create a student object. 
-	 * 	 - Call the TODO remove value from the call to the remove method. If the entry is not found, print a
-	 * 	   error message using System.err.print*
-	 * - If ADD or DEL are not found, write an error message that states it, showing the second field where 
-	 *   the token should be. 
+	 * 
+	 * @param info
 	 */
-	public static void processStudentData(String[] info, int line) {
-      boolean unique = false;
-      
-      
+	public static void processStudentData(String[] info) {
+		Student newStudent;
+		
+		if (info[1].equals("ADD")) {
 			try {
-            if (info[1].equals("ADD")) {
-				Student student = new Student(info[2],info[3],info[4],info[5]);
-            unique = List.contains(student);
-               if (unique == true) {  
-                  List.add (student);
-               } else {
-                  System.out.println ("Student information in this entry " +
-                                      "contains duplicate information." +
-                                      "\n Unable to process");
-               }
-            generateReport(student); 
-            
-         }
-         if (info[1].equals("DEL")) {
-            Student student = new Student(info[2],info[3],info[4],info[5]);
-            unique = List.contains(student);
-               if (unique == true) {  
-                  List.remove (student);
-               } else {
-                  System.out.println ("Student information in this entry " +
-                                      "contains duplicate information." +
-                                      "\n Unable to process");
-         
-               }
-         } else {
-			  System.err.println("Second entry in line, " + info[1] +
-                              " does not match either ADD or DEL." +
-                              "\n Unable to process data");          
-			  }
+				newStudent = new Student(info[2], info[3], info[4], info[5]);
+				
+				if (!listOfStudents.contains(newStudent)) {
+					
+					if(!listOfStudents.add(newStudent)) {
+						
+						System.err.println("Student \'" + newStudent.getStudentId()
+								+ "\' could not be added to the list. Skipping this entry.");
+					} else {
+						System.out.println("Student \'" + newStudent.getStudentId()
+						+ "\' was added to the list.");
+					} // End if / else
+					
+				} else {
+					System.err.println("Student \'" + newStudent.getStudentId()
+							+ "\' already exists in the list. Skipping this entry");
+					
+				} // End if / else
+				
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}  // End try / catch 
+			
+		} else if (info[1].equals("DEL")) {
+			try { 
+				newStudent = new Student(info[2], info[3], info[4], info[5]);
+				
+				if (!listOfStudents.remove(newStudent)) {
+					
+					System.err.println("Student \'" + newStudent.getStudentId()
+							+ "\' could not be removed from the list. Skipping this entry.");
+				} else {
+					System.out.println("Student \'" + newStudent.getStudentId()
+					+ "\' was removed from the list.");
+				} // End if
+				
 			} catch (Exception e) {
 				System.err.println(e.getMessage());
 			} // End try / catch
-		
-		
-	}
-   
+			
+		} else {
+			System.err.println("\'ADD\' or \'DEL\' tokens not found in line. \'" + info[1]
+					+ "\' must be one of these two tokens to be processed");
+		} // End if / else
+	} // End processStudentData
 	/**
-	 * TODO:
-	 * Does the following:
-	 * - Recieves an array of type string with grade item data 
-	 * 	- if the second field is ADD, do the following: (implemented the same as the second project)
-	 * 	- Create an instance of GradeItem
-	 * 	- Use the TODO contains method in the List class to make sure GradeItem is unique. 
-	 * 	 - If it's already in the list, display an error message that includes the GradeItem ID and student ID
-	 * 	- Call the TODO add method in the List class to add the GradeItem object to the list of GradeItems. 
-	 * 	 - If the add method's return value indicates an error, display a message with the GradeItem ID and student ID
-	 * 	- If the second field is DEL:
-	 * 	 - Create a GradeItem object. 
-	 * 	 - Call the TODO remove value from the call to the remove method. If the entry is not found, print a
-	 * 	   error message using System.err.print*
-	 * - If ADD or DEL are not found, write an error message that states it, showing the second field where 
-	 *   the token should be. 
+	 * 
+	 * @param info
 	 */
-	public static void processGradeItemData(String[] info, int line) {
-         boolean unique = false;
-      
-      
+	public static void processGradeItemData(String[] info) {
+		GradeItem newGradeItem;
+		
+		if (info[1].equals("ADD")) {
 			try {
-            if (info[1].equals("ADD")) {
-				GradeItem gradeitem = new GradeItem(info[2],info[3],info[4],info[5]);
-            unique = List.contains(gradeitem);
-               if (unique == true) {  
-                  List.add (gradeitem);
-               } else {
-                  System.out.println ("Grade Item information in this entry " +
-                                      "contains duplicate information." +
-                                      "\n Unable to process");
-               }
-            generateReport(gradeitem); 
-            
-         }
-         if (info[1].equals("DEL")) {
-            radeItem gradeitem = new GradeItem(info[2],info[3],info[4],info[5]);
-            unique = List.contains(gradeitem);
-               if (unique == true) {  
-                  List.remove (gradeitem);
-               } else {
-                  System.out.println ("Student information in this entry " +
-                                      "contains duplicate information." +
-                                      "\n Unable to process");
-         
-               }
-         } else {
-			  System.err.println("Second entry in line, " + info[1] +
-                              " does not match either ADD or DEL." +
-                              "\n Unable to process data");          
-			  }
+				newGradeItem = new GradeItem(info[3], info[4], info[5], 
+									info[6], info[2], info[7], info[8]);
+				
+				if (!listOfGradeItems.contains(newGradeItem)) {
+					
+					if (!listOfGradeItems.add(newGradeItem)) {
+						
+						System.err.println("Grade item \'" + newGradeItem.getGradeItemId()
+								+ "\' with Student ID \'" + newGradeItem.getStudentId() 
+								+ "\' could not be added to the list. Skipping this entry.");
+						
+					} else {
+						System.out.println("Grade item \'" + newGradeItem.getGradeItemId()
+						+ "\' with Student ID \'" + newGradeItem.getStudentId() 
+						+ "\' was added to the list.");
+					} // End if
+					
+				} else {
+					System.err.println("Grade item \'" + newGradeItem.getGradeItemId()
+						+ "\' with Student ID \'" + newGradeItem.getStudentId()
+						+ "\' already exists in this list. Skipping this entry.");
+				} // End if / else
+				
 			} catch (Exception e) {
 				System.err.println(e.getMessage());
-			} // End try / catch
-		
-		
-   }
+			} // End try / catch 
+			
+		} else if (info[1].equals("DEL")) {
+			try {
+				newGradeItem = new GradeItem(info[3], info[4], info[5], 
+									info[6], info[2], info[7], info[8]);
+				
+				 if (!listOfGradeItems.remove(newGradeItem)) {
+						
+						System.err.println("Grade item \'" + newGradeItem.getGradeItemId()
+							+ "\' with Student ID \'" + newGradeItem.getStudentId()
+							+ "\' cannot be deleted.");
+				 } // End if
+				
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			} // End try / catch 
+			
+		} else {
+			System.err.println("\'ADD\' or \'DEL\' tokens not found in line. \'" + info[1]
+					+ "\' must be one of these two tokens to be processed");
+		} // End if / else
+	}
 	/**
 	 * TODO:
 	 * Does the following
 	 * - Calls the TODO toArray method in the list class to get a list of Student objects
 	 * - Calls the TODO toArray method in the list class to get a list of GradeItem objects. 
-	 * - Generates a report which is writeen to an output file, Project_03_Outputxx.txt
+	 * - Generates a report which is written to an output file, Project_03_Outputxx.txt
 	 */
-	public static void generateReport(String [] item) {
+	public static void generateReport() {
+		ArrayList<Student> studentArray = new ArrayList<Student>();
+		ArrayList<GradeItem> gradeItemArray = new ArrayList<GradeItem>();
 		
-	}
+		for (Object std : listOfStudents.toArray()) {
+			if (std != null) {
+				Student student = (Student) std;
+				studentArray.add(student);
+			}
+		}
+		
+		for (Object gitm : listOfGradeItems.toArray()) {
+			if (gitm != null) {
+				GradeItem gradeitem = (GradeItem) gitm;
+				gradeItemArray.add(gradeitem);
+			}
+		}
+		
+		double averageScore;
+		int sumOfMaxScores; 
+		int sumOfAverageScores;
+		
+		
+		
+		
+	} // End generateReport
 	
-	/**
-	 * TODO:
-	 * 	- Instantiate both listOfStudents and listOfGradeItems
-	 * 	- Call the processInput method
-	 * 	- Call the generateReport method
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		listOfStudents = new List('S');
-		listOfGradeItems = new List('G');
+		listOfStudents = new List<Student>();
+		listOfGradeItems = new List<GradeItem>();
 		
 		processInput();
 
-		generateReport();
+		//generateReport();
 	}
 } // End class
